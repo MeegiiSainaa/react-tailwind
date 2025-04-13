a45746bdeefe84be729c2249021822d1fe2beecd
itworkmng <itworkllcompany@gmail.com>
2025-04-13 17:49:08 +0700

    ✨ Integration vercel

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      Tailwind Setup
+      <div className='font-bold'>New Setup [Meegii Sanaa]</div>
+    </div>
+  );
+}
+
+export default App;
